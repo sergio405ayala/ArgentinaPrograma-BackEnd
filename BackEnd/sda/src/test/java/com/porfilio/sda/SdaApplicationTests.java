1771a8509c5c39335d0dffca2af6617b8a446324
sergio405ayala <99102058+sergio405ayala@users.noreply.github.com>
2022-07-21 23:47:35 -0300

    Initial commit

---
+++ BackEnd/sda/src/test/java/com/porfilio/sda/SdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porfilio.sda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
