f7bdbf2f8137b54a529c813961d55ea48dc9385e
sergio405ayala <99102058+sergio405ayala@users.noreply.github.com>
2022-08-01 22:42:10 -0300

    actualizacion

--- BackEnd-sda/src/main/java/com/portfolio/sda/Controller/CEducacion.java
+++ BackEnd-sda/src/main/java/com/portfolio/sda/Controller/CEducacion.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://argentinaprogramafrondend.firebaseapp.com/")
 public class CEducacion {
 
     @Autowired
--- BackEnd-sda/src/main/java/com/portfolio/sda/Controller/CExperiencia.java
+++ BackEnd-sda/src/main/java/com/portfolio/sda/Controller/CExperiencia.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://argentinaprogramafrondend.firebaseapp.com/")
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
